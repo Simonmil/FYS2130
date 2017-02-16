feefe63f6d74d6e66ac1e6ab3517cce4f90b8459
Simonmil <simonmillerjord@gmail.com>
2017-02-16 12:11:06 +0100

    Programmene fungerer, men må tolke resultatene

--- Oblig4/Oblig4opg11.py
+++ Oblig4/Oblig4opg11.py
@@ -11,16 +11,21 @@ for line in fin:
 	sunspots.append(float(cols[1]))
 fin.close()
 
+times = np.array(times)
+sunspots = np.array(sunspots)
+
 plt.plot(times,sunspots,"-b")
 plt.xlabel("Tid")
 plt.ylabel("Solflekker")
 
-X = np.fft.fft(sunspots)
+X = np.fft.fft(sunspots)/(len(sunspots))
 frekv = np.linspace(0,1,len(sunspots)/2)
+#frekv = 1./times
+
 
 plt.figure()
 
-plt.plot(frekv,2*np.abs(X[0:len(sunspots)/2]))
+plt.plot(frekv[0:len(times)/2.],2*np.abs(X[0:len(sunspots)/2.]))
 
 
 plt.show()
--- Oblig4/Oblig4opg14.py
+++ Oblig4/Oblig4opg14.py
@@ -4,5 +4,21 @@ import pylab as plt
 Fs = 1000.
 delta_t = 1./Fs
 N = 512.
-t = np.linspace(0,N-1,Fs)
+t = np.linspace(0,N,Fs)
+
+f = 13./N
+
+x = np.sin(2*np.pi*f*t)
+
+plt.plot(Fs*t,x)
+
+X = np.fft.fft(x,int(N))/N
+
+frekv = (Fs/2.)*np.linspace(0,1,N/2.)
+
+plt.figure()
+
+plt.plot(frekv,2*np.abs(X[0:N/2.]))
+print f
+plt.show()
 
