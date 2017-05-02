53eb4968c5ead517e9665e6ac517cb92093b9878
Simonmil <simonmillerjord@gmail.com>
2017-05-02 13:04:32 +0100

    noe greier

--- Oblig7/Oppg14.py
+++ Oblig7/Oppg14.py
@@ -8,8 +8,8 @@ n = len(x)
 #Initial positions
 sigma = 2.0
 u = np.exp(-(x/(2*sigma))*(x/(2*sigma)))
-plt.figure()
 plt.plot(x,u,'-r')
+plt.figure(1)
 plt.show()
 
 # Generates div parameters and timederivatives of the amp at t=0
@@ -19,21 +19,25 @@ delta_x = 0.1
 factor = (delta_t*v/delta_x)**2
 dudt = (v/(2*sigma*sigma))*x*u
 
-
 # Gives effective initial conditions
 
 u_jminus1 = u - delta_t*dudt
 u_j = u
 
-u_jplus1 = np.zeros(1000)
+u_jplus1 = np.zeros(400)
 
 for i in range(len(x)):
 	if i == 0:
 		u_jplus1[i] = (2*(1-factor))*u_j[i] - u_jminus1[i] + factor*(u_j[i+1])
-	elif i == n:
-		u_jplus1[i] = (2*(1-factor))*u_j[i] - u_jminus1[i] + factor*(u_j[i+1])
+	elif i == n-1:
+		u_jplus1[i] = (2*(1-factor))*u_j[i] - u_jminus1[i] + factor*(u_j[i-1])
 	else:
-		u_jplus1[i] = (2*(1-factor))*u_j[i] - u_jminus1[i] + factor*(u_j[i+1]) + u_j[i]
+		u_jplus1[i] = (2*(1-factor))*u_j[i] - u_jminus1[i] + factor*(u_j[i+1]) + u_j[i-1]
 
+	plt.plot(u_j)
+	plt.xlim([0,n+1])
+	plt.ylim([-0.3,1.2])
 
-
+	u_jminus1 = u_j
+	u_j = u_jplus1
+plt.show()
