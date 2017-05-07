0c052c4494d438a8ecca0d62af32003bcb636761
Simonmil <simonmillerjord@gmail.com>
2017-05-07 18:54:44 +0100

    Works!

--- Prosjekt/Oppg4.py
+++ Prosjekt/Oppg4.py
@@ -1,34 +1,36 @@
 import numpy as np
 import pylab as plt
-
+from matplotlib.animation import ArtistAnimation
 
 N = 200. # masspoints
 m = np.zeros(N)
 m[1:N-1] = 0.02 # kg
-m[0] = 100. # kg
+m[0] = 100000. # kg
 m[-1] = m[0]
 k = np.zeros(N-1)
 k[:] = 10. # kg/s2
-time = np.linspace(0,1200,N-1)
-dt = np.sqrt(m[1]/k[0])*1e-3
+time = np.linspace(0,1200,N)
+dt = np.sqrt(m[1]/k[0])*1e-1
 
 
-y_0 = [np.sin(7*np.pi*i/(N-1)) for i in range(int(N)-1)]
-init = y_0
-y_0[0] = 0
-y_0[-1] = y_0[0]
-y_minus = np.zeros(N)
+
+y_0 = np.array([np.sin(7*np.pi*i/(N-1)) for i in range(int(N))])
+y_minus = np.array([np.sin(7*np.pi*i/(N-1)) for i in range(int(N))])
+
 y_pluss = np.zeros(N)
+
 all_pos = []
+anim = []
 
-
-for t in range(len(time)):
-	for i in range(1,int(N)-2):
+for t in range(1200):
+	for i in range(1,int(N)-1):
 		y_pluss[i] = (dt**2*(-(k[i-1] + k[i])*y_0[i] + k[i-1]*y_0[i-1] + k[i]*y_0[i+1]))/m[i] + 2*y_0[i] - y_minus[i]
-		y_minus[i] = y_0[i]
-		y_0[i] = y_pluss[i]
+	anim.append(plt.plot(y_0,'-b'))
+	y_minus = np.copy(y_0)
+	y_0 = np.copy(y_pluss)
 	all_pos.append(y_0)
 
-for t in range(len(time)):
-	plt.plot(init,all_pos[t])
-	plt.show()
+fig = plt.figure(1)
+plot = ArtistAnimation(fig,anim,interval=25)
+plt.show()
+
