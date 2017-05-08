3156ccafe3ee8499fec57e76e0a3f9ccb3d6e4e1
Simonmil <simonmillerjord@gmail.com>
2017-05-08 15:49:44 +0100

    Nå kommer vi noen vei! Må sjekke at plottene er riktige, og hvordan regne ut maxtiden numerisk

---
+++ Prosjekt/Oppg5.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pylab as plt
+from matplotlib.animation import ArtistAnimation
+
+N = 200. # masspoints
+m = np.zeros(N)
+m[1:N-1] = 0.02 # kg
+m[0] = 100000. # kg
+m[-1] = m[0]
+k = np.zeros(N-1)
+k[:] = 10. # kg/s2
+dt = np.sqrt(m[1]/k[0])*1e-1
+maxt = 5000
+#maxt = ((N/3.5)*np.sqrt(m[1]/k[1])*10)/dt
+
+y_0 = np.array([np.sin(7*np.pi*i/(N-1)) for i in range(int(N))])
+y_minus = np.array([np.sin(7*np.pi*i/(N-1)) for i in range(int(N))])
+
+y_pluss = np.zeros(N)
+
+pos99 = []
+anim = []
+
+count = 0
+
+for t in range(int(maxt)):
+	for i in range(1,int(N)-1):
+		y_pluss[i] = (dt**2*(-(k[i-1] + k[i])*y_0[i] + k[i-1]*y_0[i-1] + k[i]*y_0[i+1]))/m[i] + 2*y_0[i] - y_minus[i]
+		if i == 99:
+			pos99.append(y_pluss[i])
+			if y_pluss[i] > 0 and y_0[i] < 0:
+				count += 1
+			"""
+			elif y_pluss[i] < 0 and y_0[i] > 0:
+				count += 1
+			"""
+	y_minus = np.copy(y_0)
+	y_0 = np.copy(y_pluss)
+
+numfreq = count/(dt*maxt)
+print numfreq
+anim.append(plt.plot(pos99,'-b'))
+
+fftpos99 = np.fft.fft(pos99)
+plt.plot(np.abs(fftpos99)**2)
+
+fig = plt.figure(1)
+plot = ArtistAnimation(fig,anim,interval=25)
+plt.show()
+
